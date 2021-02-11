c926c3a74f76c0ab881836c1af94d152aa25d478
techcoderx <engtianxi@outlook.com>
2021-02-11 13:14:24 +0800

    example live chat browser implementation

---
+++ livechatexample/chat.js
@@ -0,0 +1,292 @@
+let gundb = new Gun(['http://localhost:3007/gun'])
+let participants = {
+    dtc: {},
+    hive: {},
+    steem: {}
+}
+let msgs = {}
+let ev = false
+let authEvs = {
+    dtc: false,
+    hive: false,
+    steem: false
+}
+let authorized = false
+let lastAccess = new Date().getTime()
+
+async function requestAccess() {
+    let ts = new Date().getTime()
+    let network = document.getElementById('usernetworkselect').value
+    let user = document.getElementById('usernameinput').value
+    let key = document.getElementById('keyinput').value
+    let streamNetwork = document.getElementById('streamnetworkselect').value
+    let streamer = document.getElementById('streamerinput').value
+    let link = document.getElementById('linkinput').value
+    if (!user || (network === 'dtc' && !key) || (network === 'steem' && !key && !window.steem_keychain) || (network === 'hive' && !key && !window.hive_keychain)) return alert('username and key is required')
+    if (await refreshAccess(true,user,network)) return
+    let hash = cg.createHashRequest(ts,user,network,streamNetwork,streamer,link)
+    let sign
+    if (network === 'dtc')
+        sign = cg.avalonSign(key,hash)
+    else if (!key)
+        try {
+            sign = await keychainSign(ts,user,network,null,streamNetwork,streamer,link)
+        } catch (e) { return alert(e) }
+    else
+        sign = cg.grapheneSign(key,hash)
+    let signed = {
+        s: network === 'dtc' ? sign.signature : sign,
+        r: network === 'dtc' ? sign.recid : undefined,
+        t: ts
+    }
+    getGunChatRequestPath().put(signed,(ack) => {
+        if (ack.err) return
+        let resp = setInterval(async () => {
+            if (await refreshAccess(true,user,network))
+                clearInterval(resp)
+        },1000)
+    })
+}
+
+function refreshAccess(alrt,user,network) {
+    return new Promise((rs) => {
+        if (!user) {
+            document.getElementById('chataccessbtn').style.display = 'inline-block'
+            return rs(false)
+        }
+        getGunChatAuthPath().get(network).get(user).once((data) => {
+            if (data) {
+                authorized = true
+                if (alrt) alert('successfully obtained authorization')
+                document.getElementById('chataccessbtn').style.display = 'none'
+            } else { 
+                authorized = false
+                document.getElementById('chataccessbtn').style.display = 'inline-block'
+            }
+            rs(authorized)
+        })
+    })
+}
+
+function fetchParticipants() {
+    return new Promise((rs) => {
+        let toFetch = {
+            dtc: [],
+            hive: [],
+            steem: []
+        }
+        getGunChatAuthPath().once(async (nets) => {
+            if (!nets) rs(participants)
+            for (let n in nets) if (n !== '_' && toFetch[n]) {
+                let netusers = await getGunItem(nets[n]['#'])
+                if (netusers && netusers._) delete netusers._
+                for (let u in netusers) if (netusers[u] !== 0)
+                    toFetch[n].push(u)
+            }
+            let keys = await getAccountKeysMulti(toFetch,true)
+            rs(keys)
+        })
+    })
+}
+
+function streamParticipants() {
+    getGunChatAuthPath().get('dtc').on(async (nets,nk,at,currentEv) => streamParticipantsHandler(nets,'dtc',currentEv))
+    getGunChatAuthPath().get('hive').on(async (nets,nk,at,currentEv) => streamParticipantsHandler(nets,'hive',currentEv))
+    getGunChatAuthPath().get('steem').on(async (nets,nk,at,currentEv) => streamParticipantsHandler(nets,'steem',currentEv))
+}
+
+async function streamParticipantsHandler(nets,network,currentEv) {
+    console.log('STREAM PARTICIPANTS',network,nets)
+    authEvs[network] = currentEv
+    let newParticipants = []
+    // todo resolve bans
+    for (let u in nets) if (u !== '_' && nets[u] !== 0 && !participants[network][u])
+        newParticipants.push(u)
+    let req = {}
+    req[network] = newParticipants
+    let newKeys = await getAccountKeysMulti(req,false)
+    Object.assign(participants[network],newKeys[network])
+}
+
+function getAccountKeysMulti(users,fetchAll) {
+    return new Promise(async (rs,rj) => {
+        // todo blockchain api config
+        let results = {
+            dtc: {},
+            hive: {},
+            steem: {}
+        }
+        for (let nets in users) {
+            if (!Array.isArray(users[nets]) || users[nets].length === 0) continue
+            if (fetchAll) for (let u in users[nets]) if (participants[nets][users[nets][u]]) users[nets].splice(u,1)
+            let d
+            if (nets === 'dtc') {
+                try {
+                    d = await axios.get('https://avalon.oneloved.tube/accounts/'+users.dtc.join(','))
+                } catch { continue }
+                // Allow master key and type 4 and 13 custom keys
+                for (let i = 0; i < d.data.length; i++) {
+                    let allowedKeys = [d.data[i].pub]
+                    for (let j in d.data[i].keys)
+                        if (d.data[i].keys[j].types.includes(4) || d.data[i].keys[j].types.includes(13))
+                            allowedKeys.push(d.data[i].keys[j].pub)
+                    results.dtc[d.data[i].name] = allowedKeys
+                }
+            } else {
+                let rpc = nets === 'hive' ? 'https://techcoderx.com' : 'https://api.steemit.com'
+                try {
+                    d = await axios.post(rpc,{
+                        id: 1,
+                        jsonrpc: '2.0',
+                        method: 'condenser_api.get_accounts',
+                        params: [users[nets]]
+                    })
+                } catch { continue }
+                if (d.data.error) { continue }
+                for (let u = 0; u < d.data.result.length; u++) {
+                    // Allow posting, active and owner keys
+                    let allowedKeys = []
+                    for (let i in d.data.result[u].posting.key_auths)
+                        allowedKeys.push(d.data.result[u].posting.key_auths[i][0])
+                    for (let i in d.data.result[u].active.key_auths)
+                        allowedKeys.push(d.data.result[u].active.key_auths[i][0])
+                    for (let i in d.data.result[u].owner.key_auths)
+                        allowedKeys.push(d.data.result[u].owner.key_auths[i][0])
+                    results[nets][d.data.result[u].name] = allowedKeys
+                }
+            }
+        }
+        rs(results)
+    })
+}
+
+async function loadChat() {
+    msgs = {}
+    participants = {
+        dtc: {},
+        hive: {},
+        steem: {}
+    }
+    document.getElementById('messages').innerText = ''
+    if (ev && ev.off && typeof ev.off === 'function') ev.off()
+    for (let i in authEvs) if (authEvs[i] && authEvs[i].off && typeof authEvs[i].off === 'function') authEvs[i].off()
+    await refreshAccess(false,document.getElementById('usernameinput').value,document.getElementById('usernetworkselect').value)
+    participants = await fetchParticipants()
+    console.log('FETCH PARTICIPANTS',participants)
+    getGunChatPath().on(async (newMsgHead,newKey,at,currentEv) => {
+        ev = currentEv
+        if (!newMsgHead) return
+        let keyObjs = JSON.parse(JSON.stringify(newMsgHead._['>']))
+        for (let k in keyObjs) if (msgs[k]) delete keyObjs[k]
+        let newSortedKeys = Object.keys(keyObjs).sort((a,b) => newMsgHead._['>'][a] - newMsgHead._['>'][b])
+        await loadMessages(newMsgHead,newSortedKeys)
+        displayMessages(newSortedKeys)
+    })
+    streamParticipants()
+}
+
+async function loadMessages(msgHead,sortedKeys) {
+    for (let i in sortedKeys) {
+        let getMsg = await getGunItem(msgHead[sortedKeys[i]]['#'])
+        if (!getMsg) continue
+        if (getMsg._) delete getMsg._
+        msgs[sortedKeys[i]] = getMsg
+        console.log('MSG',sortedKeys[i],getMsg)
+    }
+}
+
+function displayMessages(sortedKeys) {
+    for (let k in sortedKeys) if (msgs[sortedKeys[k]])
+        document.getElementById('messages').innerText = '['+msgs[sortedKeys[k]].u+'@'+msgs[sortedKeys[k]].n+'] '+msgs[sortedKeys[k]].m+'\n'+document.getElementById('messages').innerText
+}
+
+function chatEnter() {
+    let keycode = window.event.keyCode
+    if (keycode === 13 || keycode === 10)
+        sendChatMessage()
+}
+
+async function sendChatMessage() {
+    let ts = new Date().getTime()
+    let message = document.getElementById('chatmsginput').value
+    let network = document.getElementById('usernetworkselect').value
+    let user = document.getElementById('usernameinput').value
+    let key = document.getElementById('keyinput').value
+    let streamNetwork = document.getElementById('streamnetworkselect').value
+    let streamer = document.getElementById('streamerinput').value
+    let link = document.getElementById('linkinput').value
+    if (!message) return
+    if (!user || (network === 'dtc' && !key) || (network === 'steem' && !key && !window.steem_keychain) || (network === 'hive' && !key && !window.hive_keychain)) return alert('username and key is required')
+    let hash = cg.createHash(ts,user,network,message,streamNetwork,streamer,link)
+    let sign
+    if (network === 'dtc')
+        sign = cg.avalonSign(key,hash)
+    else if (!key)
+        try {
+            sign = await keychainSign(ts,user,network,message,streamNetwork,streamer,link)
+        } catch (e) { return alert(e) }
+    else
+        sign = cg.grapheneSign(key,hash)
+    let signed = {
+        u: user,
+        n: network,
+        s: network === 'dtc' ? sign.signature : sign,
+        r: network === 'dtc' ? sign.recid : undefined,
+        m: message,
+        t: ts
+    }
+    getGunChatPath().set(signed,(ack) => {
+        if (ack.ok)
+            document.getElementById('chatmsginput').value = ''
+    })
+}
+
+function keychainSign(ts,username,network,msg,streamNetwork,streamer,streamLink) {
+    let stringified = ''
+    if (msg)
+        stringified = ts+'_'+username+'_'+network+'_'+msg+'_'+streamNetwork+'/'+streamer+'/'+streamLink
+    else
+        stringified = 'alivedb_chat_request'+'_'+ts+'_'+username+'_'+network+'_'+streamNetwork+'/'+streamer+'/'+streamLink
+    return new Promise((rs) => {
+        if (network === 'steem' && !window.steem_keychain) return rj('Steem Keychain is not installed')
+        if (network === 'hive' && !window.hive_keychain) return rj('Hive Keychain is not installed')
+        let kcext = network === 'hive' ? window.hive_keychain : window.steem_keychain
+        kcext.requestSignBuffer(username,stringified,'Posting',(result) => {
+            console.log('KC SIGN',result)
+            if (!result.success) return rj(result.error)
+            rs(result.result)
+        })
+    })
+}
+
+function getGunChatPath() {
+    return gundb
+        .get('alivedb_chat')
+        .get(document.getElementById('streamnetworkselect').value)
+        .get(document.getElementById('streamerinput').value)
+        .get(document.getElementById('linkinput').value)
+}
+
+function getGunChatRequestPath() {
+    return gundb
+        .get('alivedb_chat_request')
+        .get(document.getElementById('streamnetworkselect').value)
+        .get(document.getElementById('streamerinput').value)
+        .get(document.getElementById('linkinput').value)
+        .get(document.getElementById('usernetworkselect').value)
+        .get(document.getElementById('usernameinput').value)
+}
+
+function getGunChatAuthPath() {
+    return gundb
+        .user(document.getElementById('streameralivedbpub').value)
+        .get(document.getElementById('streamnetworkselect').value+'/'+document.getElementById('streamerinput').value+'/'+document.getElementById('linkinput').value+'/participants')
+}
+
+function getGunItem(itemId) {
+    return new Promise((rs,rj) => {
+        gundb.get(itemId,(data) => {
+            rs(data.put)
+        })
+    })
+}
---
+++ livechatexample/middleware.js
@@ -0,0 +1,50 @@
+// Same middleware file as in AliveDB but striped down for browsers.
+
+// To use this middleware, import this file right after importing gundb.
+Gun.on('opt',function (ctx) {
+    if (ctx.once) return
+    this.to.next(ctx)
+
+    ctx.on('in',async function (msg) {
+        if (msg.put) {
+            let key = Object.keys(msg.put)
+            let keydet = key[0].split('/')
+            let received = msg.put[key[0]]
+            if (key.length > 0 && keydet[0] === 'alivedb_chat' && keydet.length === 5) {
+                // when screening data outside of Gun.user() namespace,
+                // the first (root) key should not start with '~'
+                // for live chat, keys would be 'alivedb_chat' -> network -> streamer -> link -> messageId
+                // message content template:
+                /*
+                {
+                    u: 'username',
+                    n: 'network (dtc, hive, steem etc)',
+                    s: 'signature',
+                    r: recid,
+                    t: timestamp,
+                    m: 'my chat message goes here'
+                }
+                */
+                if (!received.u || !received.n || !received.s || (received.n === 'dtc' && !received.r && received.r !== 0) || !received.t || !received.m) return
+                if (typeof received.u !== 'string' || typeof received.s !== 'string' || (received.r && typeof received.r !== 'number') || typeof received.t !== 'number' || typeof received.m !== 'string') return
+                if (!participants[received.n]) return
+                if (Math.abs(received.t - received._['>'].t) > 30000) return
+
+                // Recover public key from message signature
+                let pubkeystr = ''
+                try {
+                    let hash = cg.createHash(received.t,received.u,received.n,received.m,keydet[1],keydet[2],keydet[3])
+                    if (received.n === 'dtc')
+                        pubkeystr = cg.avalonRecoverFromSig(received.s,received.r,hash)
+                    else
+                        pubkeystr = cg.Signature.fromString(received.s).recover(hash)
+                } catch { return }
+
+                // Verify public key in account
+                if (!participants[received.n][received.u] || !participants[received.n][received.u].includes(pubkeystr)) return
+            }
+        }
+        // valid data received, proceed to next middleware
+        this.to.next(msg)
+    })
+})
