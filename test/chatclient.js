52ed0c6826419086377bc41f4a210f16b2d8e902
techcoderx <engtianxi@outlook.com>
2021-09-10 22:05:00 +0800

    some alivedb chat testing scripts

---
+++ test/chatclient.js
@@ -0,0 +1,29 @@
+const cg = require('./cryptography')
+const GunDB = require('gun')
+const Gun = GunDB({ peers: ['http://localhost:3007/gun'], file: 'radata2' })
+
+let message = 'hello from randomvlogs steem 2'
+let username = 'randomvlogs'
+let network = 'hive'
+let key = ''
+let ts = new Date().getTime()
+
+let streamNetwork = 'dtc'
+let streamer = 'techcoderx'
+let link = 'teststreamchat'
+
+let hash = cg.createHash(ts,username,network,message,streamNetwork,streamer,link)
+// let signature = cg.avalonSign(key,hash)
+let signature = cg.grapheneSign(key,hash)
+
+let signedObj = {
+    u: username,
+    n: network,
+    s: signature,
+    t: ts,
+    m: message
+}
+
+Gun.get('alivedb_chat').get(streamNetwork).get(streamer).get(link).set(signedObj,(ack) => {
+    console.log(ack.ok,signedObj)
+})
---
+++ test/chatrequest.js
@@ -0,0 +1,39 @@
+const GunDB = require('gun')
+const Gun = GunDB({ peers: ['http://localhost:3007/gun'], file: 'radata2' })
+// const cg = require('./cryptography')
+
+let alivedb_pk = ''
+
+// let network = 'steem'
+// let user = 'techcoderx'
+// let wif = ''
+let streamNetwork = 'dtc'
+let streamer = 'techcoderx'
+let link = 'teststreamchat'
+// let ts = new Date().getTime()
+// let hash = cg.createHashRequest(ts,user,network,streamNetwork,streamer,link)
+// let sign = cg.grapheneSign(wif,hash)
+// let sign = cg.avalonSign(wif,hash)
+// let signed = {
+//     s: sign,
+//     t: ts
+// }
+
+// Gun.get('alivedb_chat_request').get(streamNetwork).get(streamer).get(link).get(network).get(user).put(signed,(ack) => {
+//     console.log(signed,ack.ok)
+//     Gun.user(alivedb_pk).get(`${streamNetwork}/${streamer}/${link}/participants`).get(network).get(user).on((data) => console.log('response',data))
+// })
+
+// Gun.get('alivedb_chat_request').get('dtc').get('techcoderx').get('teststreamchat').put(5003,(ack) => {
+//     console.log('ack ok',ack.ok)
+//     Gun.get('alivedb_chat_request').get('dtc').get('techcoderx').get('teststreamchat2').once((data) => {
+//         console.log('data',data)
+//     })
+// })
+
+/*
+0: Banned/Muted
+1: Approved
+*/
+
+Gun.user(alivedb_pk).get('dtc/techcoderx/teststreamchat/participants').get('hive').get('randomvlogs').once((val) => console.log('VAL',val))
