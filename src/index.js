2697d3114a5d033eff6412f455656ccc20f0a2df
techcoderx <engtianxi@outlook.com>
2023-06-29 20:43:03 +0800

    process sigterm
    docker stop hangs without it

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ const Config = require('./config')
 const server = require('./server')
 
 server.listen(Config.http_port,Config.http_host,() => console.log(`AliveDB API server listening on port ${Config.http_port}`))
+
+// called by docker stop
+process.on('SIGTERM', () => process.exit(0))
